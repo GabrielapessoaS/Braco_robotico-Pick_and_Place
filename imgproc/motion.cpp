7cbf49cb92d7644b3c394f9606b15d19dcdc7e08
Gabriel Williams <gabriel.williams44@gmail.com>
2020-11-14 19:19:06 +0000

    Problema de reconhecimento de objetos resolvido na Rasp. Adicionado parametro iteracao2.

--- imgproc/motion.cpp
+++ imgproc/motion.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	if(argc < 4) {
-		cerr << "Uso: " << argv[0] << " id_camera sensibilidade iteracoes\n";
+		cerr << "Uso: " << argv[0] << " id_camera sensibilidade iteracoes1 iteracoes2\n";
 		cerr << "\n\tPara informacoes no significado dos parametros, consulte o README.md\n";
 		return -1;
 	}
@@ -35,6 +35,9 @@ int main(int argc, char* argv[]) {
 			return 0;	
 		}
 		pBackSub->apply(frame, fgMask);
+		cv::imshow("Plano de fundo", fgMask);
+		if(cv::waitKey(5) >= 0)
+			break;
 	}
 	cout << "Reconhecimento finalizado. Posicione os objetos e pressione ENTER.\n";
 	cv::imshow("Plano de fundo", fgMask);
@@ -42,52 +45,58 @@ int main(int argc, char* argv[]) {
 	if(keyboard == 'q')
 		return -1;
 	cv::destroyWindow("Plano de fundo");
-	//while(1) {
+	for(int i=0; i<atoi(argv[4]); i++) {
 		capture >> frame;
+		cv::imshow("what", frame);
+		if(cv::waitKey(5) >= 0) {
+			break;
+		}
+			
 		if(frame.empty()) {
 			cerr << "empty frame\n";
 			return -1; //break;
 		}
-		pBackSub->apply(frame, fgMask);
-		cv::imshow("Objetos", fgMask);
+	}
+	cv::destroyWindow("what");
+	pBackSub->apply(frame, fgMask);
+	cv::imshow("Objetos", fgMask);
 
-		cv::RNG rng(12345);
-		cv::findContours(fgMask, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE, cv::Point(0,0));
+	cv::RNG rng(12345);
+	cv::findContours(fgMask, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE, cv::Point(0,0));
 
-		vector<cv::Rect> boundRect(contours.size());
-		vector<vector<cv::Point>> contours_poly(contours.size());
-		vector<cv::Moments> m(contours.size());
-		vector<cv::Point> centers(contours.size());
+	vector<cv::Rect> boundRect(contours.size());
+	vector<vector<cv::Point>> contours_poly(contours.size());
+	vector<cv::Moments> m(contours.size());
+	vector<cv::Point> centers(contours.size());
 
-		char name[20];
-		int valid_contours=0;
-		for(int i=0; i<contours.size(); i++) {
-			if(cv::contourArea(contours[i]) < atoi(argv[2]))
-				continue;
-			valid_contours++;
-			cv::putText(frame, "Objetos reconhecidos", cv::Point(10, 20), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0, 0, 255), 2);
-			cv::approxPolyDP(contours[i], contours_poly[i], 3, true);
+	char name[20];
+	int valid_contours=0;
+	for(int i=0; i<contours.size(); i++) {
+		if(cv::contourArea(contours[i]) < atoi(argv[2]))
+			continue;
+		valid_contours++;
+		cv::putText(frame, "Objetos reconhecidos", cv::Point(10, 20), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0, 0, 255), 2);
+		cv::approxPolyDP(contours[i], contours_poly[i], 3, true);
 
-			boundRect[i] = cv::boundingRect(contours_poly[i]);
-			cv::Scalar color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-			cv::rectangle(frame, boundRect[i].tl(), boundRect[i].br(), color, 2);
+		boundRect[i] = cv::boundingRect(contours_poly[i]);
+		cv::Scalar color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+		cv::rectangle(frame, boundRect[i].tl(), boundRect[i].br(), color, 2);
 
-			m[i] = cv::moments(contours[i], true);
-			centers[i] = cv::Point(m[i].m10/m[i].m00, m[i].m01/m[i].m00);
+		m[i] = cv::moments(contours[i], true);
+		centers[i] = cv::Point(m[i].m10/m[i].m00, m[i].m01/m[i].m00);
 
-			cv::circle(frame, centers[i], 3, cv::Scalar(128,0,0), -1);
+		cv::circle(frame, centers[i], 3, cv::Scalar(128,0,0), -1);
 
-			sprintf(name, "%d:(%d,%d)", valid_contours, centers[i].x, centers[i].y);
-			cv::putText(frame, string(name), centers[i], cv::FONT_HERSHEY_SIMPLEX, 0.50, cv::Scalar(10, 0, 255), 2);
+		sprintf(name, "%d:(%d,%d)", valid_contours, centers[i].x, centers[i].y);
+		cv::putText(frame, string(name), centers[i], cv::FONT_HERSHEY_SIMPLEX, 0.50, cv::Scalar(10, 0, 255), 2);
 
-		}
-		cout << valid_contours << " objetos reconhecidos.\n";
+	}
+	cout << valid_contours << " objetos reconhecidos.\n";
 
-		cv::imshow("Stream", frame);
-		keyboard = cv::waitKey(0);
-		if(keyboard == 'q' || keyboard == 27)
-			return -1; //break;
-	//}
+	cv::imshow("Stream", frame);
+	keyboard = cv::waitKey(0);
+	if(keyboard == 'q' || keyboard == 27)
+		return -1;
 
 	return 0;
 }
